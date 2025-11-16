f06af08d257d1e392f7d504d9af15ad4101a7f7f
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-11-16 16:18:33 -0300

    feat: Rename files and folders

---
+++ 01_create_tables_and_databases/02_create_table_directors.sql
@@ -0,0 +1,9 @@
+CREATE TABLE directors (
+	director_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(150),
+	last_namre VARCHAR(150),
+	date_of_birth DATE,
+	nationality VARCHAR(20),
+	add_date DATE,
+	update_date DATE
+);
